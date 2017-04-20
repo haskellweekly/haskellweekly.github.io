d2b0ff63a640bb39a282a548d798d803f0a9e219
Taylor Fausak <taylor@fausak.me>
2017-04-20 09:15:59 -0500

    Rewrite deploy script in Haskell

---
+++ tools/deploy.hs
@@ -0,0 +1,36 @@
+#!/usr/bin/env stack
+{-
+  stack
+  --resolver lts-8.0
+  script
+  --package directory
+  --package filepath
+  --package process
+-}
+
+import qualified Control.Monad as Monad
+import qualified System.Directory as Directory
+import qualified System.Environment as Environment
+import qualified System.FilePath as FilePath
+import qualified System.Process as Process
+
+main :: IO ()
+main = do
+  token <- Environment.getEnv "GITHUB_TOKEN"
+  branch <- Environment.getEnv "TRAVIS_BRANCH"
+  commit <- Environment.getEnv "TRAVIS_COMMIT"
+  isPullRequest <- Environment.getEnv "TRAVIS_PULL_REQUEST"
+
+  Monad.guard (branch == "hakyll")
+  Monad.guard (isPullRequest == "false")
+  
+  Directory.setCurrentDirectory (FilePath.joinPath ["_hakyll", "site"])
+  let git = Process.callProcess "git"
+
+  git ["init"]
+  git ["add", "."]
+  git ["config", "--global", "user.email", "taylor@fausak.me"]
+  git ["config", "--global", "user.name", "Taylor Fausak"]
+  git ["commit", "--author", "Haskell Weekly <info@haskellweekly.news>", "--message", "Automatic deploy of " ++ commit]
+  git ["remote", "add", "origin", "https://" ++ token ++ "@github.com/haskellweekly/haskellweekly.github.io.git"]
+  git ["push", "--force", "--quiet", "origin", "master"]
