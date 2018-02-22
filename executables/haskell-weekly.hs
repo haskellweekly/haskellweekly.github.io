ca4cf7d863a13dcd06ab000904491de6d63b5c6b
Taylor Fausak <taylor@fausak.me>
2018-02-22 10:41:50 -0500

    Format everything with Brittany

--- executables/haskell-weekly.hs
+++ executables/haskell-weekly.hs
@@ -24,7 +24,8 @@ main = do
   Directory.removePathForcibly output
 
   -- Create the expected hierarchy in the output directory.
-  mapM_ createDirectoryAt
+  mapM_
+    createDirectoryAt
     [ [output]
     , [output, "images"]
     , [output, "issues"]
@@ -32,7 +33,8 @@ main = do
     ]
 
   -- Copy over static files.
-  mapM_ (copyFileAt input output)
+  mapM_
+    (copyFileAt input output)
     [ ["images", "favicon.ico"]
     , ["images", "twitter-card.png"]
     ]
@@ -101,29 +103,36 @@ main = do
 
   -- Create home page.
   do
-    contents <- renderIndex baseTemplate indexTemplate snippetTemplate context issues
+    contents <- renderIndex
+      baseTemplate
+      indexTemplate
+      snippetTemplate
+      context
+      issues
     writeFileAt [output, "index.html"] contents
 
 getSurveysByYear :: FilePath -> [FilePath] -> IO [(Integer, Text)]
 getSurveysByYear input =
-  mapM (getSurvey input) .
-  Maybe.mapMaybe (Read.readMaybe . FilePath.takeBaseName) .
-  filter (hasExtension "html")
+  mapM (getSurvey input)
+    . Maybe.mapMaybe (Read.readMaybe . FilePath.takeBaseName)
+    . filter (hasExtension "html")
 
 getSurvey :: FilePath -> Integer -> IO (Integer, Text)
 getSurvey input year = do
-  template <- readFileAt [input, "surveys", FilePath.addExtension (show year) "html"]
+  template <- readFileAt
+    [input, "surveys", FilePath.addExtension (show year) "html"]
   pure (year, template)
 
 getIssuesByNumber :: FilePath -> [FilePath] -> IO [(Integer, Text)]
 getIssuesByNumber input =
-  mapM (getIssue input) .
-  Maybe.mapMaybe (Read.readMaybe . FilePath.takeBaseName) .
-  filter (hasExtension "markdown")
+  mapM (getIssue input)
+    . Maybe.mapMaybe (Read.readMaybe . FilePath.takeBaseName)
+    . filter (hasExtension "markdown")
 
 getIssue :: FilePath -> Integer -> IO (Integer, Text)
 getIssue input number = do
-  contents <- readFileAt [input, "issues", FilePath.addExtension (show number) "markdown"]
+  contents <- readFileAt
+    [input, "issues", FilePath.addExtension (show number) "markdown"]
   pure (number, contents)
 
 createIssue :: FilePath -> Text -> Text -> Context -> Issue -> IO ()
@@ -135,7 +144,9 @@ createIssue output baseTemplate issueTemplate context issue = do
 createSurvey :: FilePath -> Text -> Text -> Context -> (Integer, Text) -> IO ()
 createSurvey output baseTemplate surveyTemplate context (year, template) = do
   contents <- renderSurvey baseTemplate surveyTemplate template context year
-  writeFileAt [output, "surveys", FilePath.addExtension (show year) "html"] contents
+  writeFileAt
+    [output, "surveys", FilePath.addExtension (show year) "html"]
+    contents
 
 -- Types
 
@@ -160,25 +171,22 @@ escapeHtml :: Text -> Text
 escapeHtml = Text.replace "<" "&lt;" . Text.replace "&" "&amp;"
 
 getDay :: Text -> Maybe Time.Day
-getDay meta =
-  case Text.words meta of
-    ["<!--", day, "-->"] -> parseDay "%Y-%m-%d" day
-    _ -> Nothing
+getDay meta = case Text.words meta of
+  ["<!--", day, "-->"] -> parseDay "%Y-%m-%d" day
+  _ -> Nothing
 
 isoDay :: Time.Day -> Text
 isoDay = formatDay "%Y-%m-%dT00:00:00Z"
 
 issueContext :: Issue -> Context
-issueContext issue =
-  [ ("number", showText (issueNumber issue))
-  ]
+issueContext issue = [("number", showText (issueNumber issue))]
 
 issueSummary :: Monad m => Issue -> m Text
 issueSummary =
   renderTemplate
-    "Issue $number$ of Haskell Weekly, a free email newsletter about the \
-    \Haskell programming language." .
-  issueContext
+      "Issue $number$ of Haskell Weekly, a free email newsletter about the \
+      \Haskell programming language."
+    . issueContext
 
 issueTitle :: Monad m => Issue -> m Text
 issueTitle = renderTemplate "Issue $number$" . issueContext
@@ -188,24 +196,24 @@ issueUrl = renderTemplate "/issues/$number$.html" . issueContext
 
 lastUpdated :: [Issue] -> Time.Day
 lastUpdated =
-  Maybe.fromMaybe (Time.fromGregorian 1970 1 1) .
-  Maybe.listToMaybe . map issueDay
+  Maybe.fromMaybe (Time.fromGregorian 1970 1 1)
+    . Maybe.listToMaybe
+    . map issueDay
 
 pageTitle :: Monad m => Maybe Text -> m Text
-pageTitle maybeTitle =
-  case maybeTitle of
-    Nothing -> pure "Haskell Weekly"
-    Just title -> renderTemplate "$title$ :: Haskell Weekly"
-      [ ("title", title)
-      ]
+pageTitle maybeTitle = case maybeTitle of
+  Nothing -> pure "Haskell Weekly"
+  Just title -> renderTemplate "$title$ :: Haskell Weekly" [("title", title)]
 
 parseIssue :: (Integer, Text) -> Maybe Issue
 parseIssue (number, contents) = do
   let (meta, body) = Text.breakOn "\n" contents
   day <- getDay meta
-  Just
-    Issue
-    {issueNumber = number, issueDay = day, issueContents = commonMark body}
+  Just Issue
+    { issueNumber = number
+    , issueDay = day
+    , issueContents = commonMark body
+    }
 
 prettyDay :: Time.Day -> Text
 prettyDay = formatDay "%B %e %Y"
@@ -217,16 +225,14 @@ sortIssues :: [Issue] -> [Issue]
 sortIssues = List.sortBy (Ord.comparing (Ord.Down . issueDay))
 
 surveyContext :: Integer -> Context
-surveyContext year =
-  [ ("year", showText year)
-  ]
+surveyContext year = [("year", showText year)]
 
 surveySummary :: Monad m => Integer -> m Text
 surveySummary =
   renderTemplate
-    "The $year$ survey of Haskell users by Haskell Weekly, a free email \
-    \newsletter about the Haskell programming language." .
-  surveyContext
+      "The $year$ survey of Haskell users by Haskell Weekly, a free email \
+      \newsletter about the Haskell programming language."
+    . surveyContext
 
 surveyTitle :: Monad m => Integer -> m Text
 surveyTitle = renderTemplate "$year$ survey" . surveyContext
@@ -240,28 +246,36 @@ renderAdvertising :: Monad m => Text -> Text -> Context -> m Text
 renderAdvertising template advertisingTemplate context = do
   body <- renderTemplate advertisingTemplate context
   title <- pageTitle (Just "Advertising")
-  renderTemplate template (context ++
-    [ ("body", body)
-    , ("summary", "Information about advertising with Haskell Weekly.")
-    , ("title", title)
-    , ("url", "/advertising.html")
-    ])
+  renderTemplate
+    template
+    ( context
+    ++ [ ("body", body)
+       , ("summary", "Information about advertising with Haskell Weekly.")
+       , ("title", title)
+       , ("url", "/advertising.html")
+       ]
+    )
 
 renderAtom :: Monad m => Text -> Text -> Context -> [Issue] -> m Text
 renderAtom template entryTemplate context issues = do
   entries <- mapM (renderAtomEntry entryTemplate context) issues
-  renderTemplate template (context ++
-    [ ("entries", mconcat entries)
-    , ("updated", isoDay (lastUpdated issues))
-    ])
+  renderTemplate
+    template
+    ( context
+    ++ [ ("entries", mconcat entries)
+       , ("updated", isoDay (lastUpdated issues))
+       ]
+    )
 
 renderAtomEntry :: Monad m => Text -> Context -> Issue -> m Text
-renderAtomEntry template context issue =
-  renderTemplate template (context ++
-    [ ("content", escapeHtml (issueContents issue))
-    , ("number", showText (issueNumber issue))
-    , ("updated", isoDay (issueDay issue))
-    ])
+renderAtomEntry template context issue = renderTemplate
+  template
+  ( context
+  ++ [ ("content", escapeHtml (issueContents issue))
+     , ("number", showText (issueNumber issue))
+     , ("updated", isoDay (issueDay issue))
+     ]
+  )
 
 renderIndex :: Monad m => Text -> Text -> Text -> Context -> [Issue] -> m Text
 renderIndex baseTemplate template snippetTemplate context issues = do
@@ -269,44 +283,43 @@ renderIndex baseTemplate template snippetTemplate context issues = do
   body <- renderTemplate template (("issues", mconcat snippets) : context)
   let
     summary :: Text
-    summary =
-      "Haskell Weekly is a free email newsletter about the Haskell \
-      \programming language. Each issue features several hand-picked links to \
-      \interesting content about Haskell from around the web."
+    summary
+      = "Haskell Weekly is a free email newsletter about the Haskell \
+        \programming language. Each issue features several hand-picked links \
+        \to interesting content about Haskell from around the web."
   title <- pageTitle Nothing
-  renderTemplate baseTemplate (context ++
-    [ ("body", body)
-    , ("summary", summary)
-    , ("title", title)
-    , ("url", "")
-    ])
+  renderTemplate
+    baseTemplate
+    ( context
+    ++ [("body", body), ("summary", summary), ("title", title), ("url", "")]
+    )
 
 renderIssue :: Monad m => Text -> Text -> Context -> Issue -> m Text
 renderIssue baseTemplate issueTemplate context issue = do
   partialTitle <- issueTitle issue
-  body <- renderTemplate issueTemplate (context ++
-    [ ("body", issueContents issue)
-    , ("date", prettyDay (issueDay issue))
-    , ("title", partialTitle)
-    ])
+  body <- renderTemplate
+    issueTemplate
+    ( context
+    ++ [ ("body", issueContents issue)
+       , ("date", prettyDay (issueDay issue))
+       , ("title", partialTitle)
+       ]
+    )
   summary <- issueSummary issue
   url <- issueUrl issue
   title <- pageTitle (Just partialTitle)
-  renderTemplate baseTemplate (context ++
-    [ ("body", body)
-    , ("summary", summary)
-    , ("title", title)
-    , ("url", url)
-    ])
+  renderTemplate
+    baseTemplate
+    ( context
+    ++ [("body", body), ("summary", summary), ("title", title), ("url", url)]
+    )
 
 renderPiece :: Monad m => Context -> Piece -> m Text
-renderPiece context piece =
-  case piece of
-    Literal text -> pure text
-    Variable name ->
-      case lookup name context of
-        Nothing -> fail ("unknown variable: " ++ show name)
-        Just value -> pure value
+renderPiece context piece = case piece of
+  Literal text -> pure text
+  Variable name -> case lookup name context of
+    Nothing -> fail ("unknown variable: " ++ show name)
+    Just value -> pure value
 
 renderPieces :: Monad m => Context -> [Piece] -> m Text
 renderPieces context pieces = do
@@ -322,40 +335,41 @@ renderRssItem :: Monad m => Text -> Context -> Issue -> m Text
 renderRssItem template context issue = do
   title <- issueTitle issue
   url <- issueUrl issue
-  renderTemplate template (context ++
-    [ ("description", escapeHtml (issueContents issue))
-    , ("pubDate", rfcDay (issueDay issue))
-    , ("title", title)
-    , ("url", url)
-    ])
+  renderTemplate
+    template
+    ( context
+    ++ [ ("description", escapeHtml (issueContents issue))
+       , ("pubDate", rfcDay (issueDay issue))
+       , ("title", title)
+       , ("url", url)
+       ]
+    )
 
 renderSnippet :: Monad m => Text -> Context -> Issue -> m Text
 renderSnippet template context issue = do
   title <- issueTitle issue
   url <- issueUrl issue
-  renderTemplate template (context ++
-    [ ("date", prettyDay (issueDay issue))
-    , ("title", title)
-    , ("url", url)
-    ])
+  renderTemplate
+    template
+    ( context
+    ++ [("date", prettyDay (issueDay issue)), ("title", title), ("url", url)]
+    )
 
 renderSurvey :: Monad m => Text -> Text -> Text -> Context -> Integer -> m Text
 renderSurvey baseTemplate surveyTemplate template context year = do
   partialBody <- renderTemplate template context
   partialTitle <- surveyTitle year
-  body <- renderTemplate surveyTemplate (context ++
-    [ ("body", partialBody)
-    , ("title", partialTitle)
-    ])
+  body <- renderTemplate
+    surveyTemplate
+    (context ++ [("body", partialBody), ("title", partialTitle)])
   summary <- surveySummary year
   title <- pageTitle (Just partialTitle)
   url <- surveyUrl year
-  renderTemplate baseTemplate (context ++
-    [ ("body", body)
-    , ("summary", summary)
-    , ("title", title)
-    , ("url", url)
-    ])
+  renderTemplate
+    baseTemplate
+    ( context
+    ++ [("body", body), ("summary", summary), ("title", title), ("url", url)]
+    )
 
 renderTemplate :: Monad m => Text -> Context -> m Text
 renderTemplate template context = renderPieces context (toPieces template)
@@ -364,35 +378,37 @@ toPieces :: Text -> [Piece]
 toPieces = textsToPieces . Text.splitOn "$"
 
 textsToPieces :: [Text] -> [Piece]
-textsToPieces chunks =
-  case chunks of
-    [] -> []
-    [text] -> [Literal text]
-    text:name:rest -> Literal text : Variable name : textsToPieces rest
+textsToPieces chunks = case chunks of
+  [] -> []
+  [text] -> [Literal text]
+  text:name:rest -> Literal text : Variable name : textsToPieces rest
 
 -- Generic helpers
 
 copyFileAt :: FilePath -> FilePath -> [FilePath] -> IO ()
-copyFileAt input output path =
-  Directory.copyFile
-    (FilePath.joinPath (input : path))
-    (FilePath.joinPath (output : path))
+copyFileAt input output path = Directory.copyFile
+  (FilePath.joinPath (input : path))
+  (FilePath.joinPath (output : path))
 
 createDirectoryAt :: [FilePath] -> IO ()
-createDirectoryAt = Directory.createDirectoryIfMissing True . FilePath.joinPath
+createDirectoryAt =
+  Directory.createDirectoryIfMissing True . FilePath.joinPath
 
 formatDay :: Text -> Time.Day -> Text
-formatDay format = Text.pack . Time.formatTime Time.defaultTimeLocale (Text.unpack format)
+formatDay format =
+  Text.pack . Time.formatTime Time.defaultTimeLocale (Text.unpack format)
 
 hasExtension :: Text -> FilePath -> Bool
-hasExtension extension = (== '.' : Text.unpack extension) . FilePath.takeExtension
+hasExtension extension =
+  (== '.' : Text.unpack extension) . FilePath.takeExtension
 
 listDirectoryAt :: [FilePath] -> IO [FilePath]
 listDirectoryAt = Directory.listDirectory . FilePath.joinPath
 
 parseDay :: Text -> Text -> Maybe Time.Day
 parseDay format =
-  Time.parseTimeM False Time.defaultTimeLocale (Text.unpack format) . Text.unpack
+  Time.parseTimeM False Time.defaultTimeLocale (Text.unpack format)
+    . Text.unpack
 
 readFileAt :: [FilePath] -> IO Text
 readFileAt path = do
