09e633c0227008f72188982ba749df28688907c7
Taylor Fausak <taylor@fausak.me>
2018-02-22 10:09:04 -0500

    Install a specific version of Stack

--- tools/install-stack.sh
+++ tools/install-stack.sh
@@ -3,7 +3,7 @@ set -o errexit -o xtrace
 
 if ! test -f "$HOME/.local/bin/stack"
 then
-  URL='https://www.stackage.org/stack/linux-x86_64'
+  URL='https://github.com/commercialhaskell/stack/releases/download/v1.6.5/stack-1.6.5-linux-x86_64.tar.gz'
   curl --location "$URL" --output stack.tar.gz
   gunzip stack.tar.gz
   tar --extract --file stack.tar --strip-components 1 --wildcards '*/stack'
