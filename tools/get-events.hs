437aebd86936e6a881dfc94f40f8152c28031efc
Taylor Fausak <taylor@fausak.me>
2018-08-15 21:45:12 -0400

    Update script for getting events

--- tools/get-events.hs
+++ tools/get-events.hs
@@ -1,5 +1,6 @@
 #!/usr/bin/env stack
 -- stack --resolver lts-12.0 script
+{-# OPTIONS_GHC -Weverything -Wno-implicit-prelude -Wno-unsafe #-}
 
 module Main
   ( main
@@ -9,6 +10,7 @@ where
 import qualified Control.Arrow as Arrow
 import qualified Data.Aeson as Aeson
 import qualified Data.Aeson.Types as Aeson
+import qualified Data.Char as Char
 import qualified Data.List as List
 import qualified Data.Maybe as Maybe
 import qualified Data.Text as Text
@@ -45,20 +47,14 @@ printEvent :: Event -> IO ()
 printEvent event = putStrLn $ concat
   [ "- "
   , formatDate $ eventLocalDate event
+  , " in "
+  , eventLocation event
+  , " by "
+  , groupName $ eventGroup event
   , ": ["
   , eventName event
   , "]("
   , eventLink event
-  , ") hosted by ["
-  , groupName $ eventGroup event
-  , "](https://www.meetup.com/"
-  , groupUrlname $ eventGroup event
-  , "/) in ["
-  , eventLocation event
-  , "](https://www.openstreetmap.org/?mlat="
-  , show $ eventLatitude event
-  , "&mlon="
-  , show $ eventLongitude event
   , ")"
   ]
 
@@ -73,8 +69,7 @@ formatDay :: Time.Day -> String
 formatDay = Time.formatTime Time.defaultTimeLocale "%FT00:00:00"
 
 formatDate :: Date -> String
-formatDate (Date day) =
-  Time.formatTime Time.defaultTimeLocale "%A, %B %-d" day
+formatDate (Date day) = Time.formatTime Time.defaultTimeLocale "%F" day
 
 newtype Payload = Payload
   { payloadEvents :: [Event]
@@ -108,50 +103,37 @@ eventLocation event =
       (groupLocalizedLocation $ eventGroup event)
       (\venue -> List.intercalate ", " $ Maybe.catMaybes
         [ Just $ venueCity venue
-        , venueState venue
+        , capitalizeState <$> venueState venue
         , Just $ venueLocalizedCountryName venue
         ]
       )
     $ eventVenue event
 
-eventLatitude :: Event -> Double
-eventLatitude event =
-  maybe (groupLat $ eventGroup event) venueLat $ eventVenue event
-
-eventLongitude :: Event -> Double
-eventLongitude event =
-  maybe (groupLon $ eventGroup event) venueLon $ eventVenue event
+capitalizeState :: String -> String
+capitalizeState state = case state of
+  [_, _] -> fmap Char.toUpper state
+  _ -> state
 
 data Group = Group
-  { groupLat :: Double
-  , groupLon :: Double
-  , groupName :: String
+  { groupName :: String
   , groupLocalizedLocation :: String
-  , groupUrlname :: String
   }
 
 instance Aeson.FromJSON Group where
   parseJSON = Aeson.withObject "Group" $ \object -> Group
-    <$> required object "lat"
-    <*> required object "lon"
-    <*> required object "name"
+    <$> required object "name"
     <*> required object "localized_location"
-    <*> required object "urlname"
 
 data Venue = Venue
   { venueCity :: String
-  , venueLat :: Double
   , venueLocalizedCountryName :: String
-  , venueLon :: Double
   , venueState :: Maybe String
   }
 
 instance Aeson.FromJSON Venue where
   parseJSON = Aeson.withObject "Venue" $ \object -> Venue
     <$> required object "city"
-    <*> required object "lat"
     <*> required object "localized_country_name"
-    <*> required object "lon"
     <*> optional object "state"
 
 required :: Aeson.FromJSON a => Aeson.Object -> String -> Aeson.Parser a
