913ce2be15c516740ec97374a5d7a86fd400ef1e
Taylor Fausak <taylor@fausak.me>
2018-10-17 22:19:32 -0400

    Upgrade to Stack 1.9.1

--- tools/install-stack.sh
+++ tools/install-stack.sh
@@ -3,7 +3,8 @@ set -o errexit -o xtrace
 
 if ! test -f "$HOME/.local/bin/stack"
 then
-  URL='https://github.com/commercialhaskell/stack/releases/download/v1.6.5/stack-1.6.5-linux-x86_64.tar.gz'
+  VERSION=1.9.1
+  URL="https://github.com/commercialhaskell/stack/releases/download/v$VERSION/stack-$VERSION-linux-x86_64.tar.gz"
   curl --location "$URL" --output stack.tar.gz
   gunzip stack.tar.gz
   tar --extract --file stack.tar --strip-components 1 --wildcards '*/stack'
